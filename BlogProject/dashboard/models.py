8dd12cc593afb83b4cbd9b9d6c6955045d5b1582
Shakil Ahmmed <shakilfci461@gmail.com>
2019-04-02 22:05:48 +0600

    Category ADDED

--- BlogProject/dashboard/models.py
+++ BlogProject/dashboard/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name=models.CharField(max_length=50,unique=True)
+    category_description=models.TextField()
+    category_status=models.CharField(max_length=20)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=255)
