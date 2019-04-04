b846bd22a1228dc3d4b83fcb1af5569da4471c87
Shakil Ahmmed <shakilfci461@gmail.com>
2019-04-05 02:06:05 +0600

    Redirect Dashboard If User Is Authenticated With Messages

--- BlogProject/dashboard/models.py
+++ BlogProject/dashboard/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class Category(models.Model):
-    category_name=models.CharField(max_length=50,unique=True)
-    category_description=models.TextField()
-    category_status=models.CharField(max_length=20)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
+    category_name = models.CharField(max_length=50, unique=True)
+    category_description = models.TextField()
+    category_status = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.category_name
