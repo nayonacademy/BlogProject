2ea244b89b96c52f011ade943c12881202640ce3
kopika <taniafci81@gmail.com>
2019-04-11 17:30:58 +0600

    Category update

--- BlogProject/dashboard/views.py
+++ BlogProject/dashboard/views.py
@@ -109,7 +109,21 @@ def category(request):
         }
         return render(request, 'dashboard/create_category.html', context)
 
-
+def category_update(request,pk):
+    if request.method == 'POST':
+        if form.is_valid():
+            category_data=get_object_or_404(Category,pk=pk)
+            form=CategoryForm(request.POST,instance=category_data)
+            form.save()
+            return HttpResponseRedirect(reverse('category'))
+    if request.method == 'GET':
+        category=get_object_or_404(Category,pk=pk)
+        form=CategoryForm(request.POST or None,instance=category)
+        print(form)
+        context={
+            'form':form
+        }
+        return render(request,'dashboard/category_edit.html',context)    
 @login_required
 def category_delete(request, pk):
     category = Category.objects.filter(pk=pk).delete()
